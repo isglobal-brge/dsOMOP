d287201218820f018402f45aba66e0146d13fa2b
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-01-17 13:25:59 +0100

    fix: filter source in query

--- R/getTable.R
+++ R/getTable.R
@@ -4,9 +4,13 @@
 #' @param tableName The name of the table to be retrieved.
 #' @return A data frame representing the table, excluding any 'source' columns.
 getTable <- function(connection, tableName) {
-  # Read the table from the database
-  table <- DBI::dbReadTable(connection, tableName)
+  # Get all column names from the table
+  allColumns <- DBI::dbListFields(connection, tableName)
   # Exclude any columns named 'source'
-  table <- table[, !grepl("source", names(table))]
+  selectedColumns <- allColumns[!grepl("source", allColumns)]
+  # Create a SQL query to select only the desired columns
+  query <- paste0("SELECT ", paste(selectedColumns, collapse = ", "), " FROM ", tableName)
+  # Execute the query and return the result
+  table <- DBI::dbGetQuery(connection, query)
   return(table)
 }
