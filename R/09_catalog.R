cfdddadd6bc4f145520ca4a7ab6c8140e4b900b4
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-02-22 02:52:06 +0100

    feat: object creation checks

--- R/09_catalog.R
+++ R/09_catalog.R
@@ -89,54 +89,92 @@ getConceptCatalogDS <- function(resource, tableName) {
   connection <- getConnection(resource)
 
   # Attempts to retrieve the concept catalog from the specified table
-  tryCatch({
-    # Finds the case-insensitive table name to ensure correct retrieval
-    tables <- getTables(connection)
-    caseInsensitiveTableName <- findCaseInsensitiveTable(tables, tableName)
-    if (is.null(caseInsensitiveTableName)) {
-      stop(paste0("The table '", tableName, "' does not exist in the database."))
-    }
-    tableName <- caseInsensitiveTableName
-
-    # Attempts to identify the 'concept' table in the database
-    tables <- getTables(connection)
-    conceptTable <- findCaseInsensitiveTable(tables, "concept")
-    
-    # Gets the required column names for this operation
-    columns <- getColumns(connection, tableName)
-    conceptIdColumn <- getConceptIdColumn(tableName)
-
-    # Checks if the concept ID column exists in the table
-    if (!conceptIdColumn %in% columns) {
-      stop(paste0("The column '", conceptIdColumn, "' does not exist in the table '", tableName, "'."))
-    }
-
-    # Retrieves the unique concept IDs from the table
-    conceptIds <- DBI::dbGetQuery(connection, paste0("SELECT DISTINCT ", conceptIdColumn, " FROM ", tableName))
-    conceptIds <- conceptIds[[1]]
-
-    # Retrieves the concepts from the 'concept' table
-    conceptCatalog <- tryCatch(
-      {
-        getConcepts(connection, conceptIds, conceptTable)
-      },
-      # In case of an error, returns an empty data frame (with the same structure as the expected output)
-      error = function(error) {
-        data.frame(concept_id = numeric(0), concept_name = character(0))
+  tryCatch(
+    {
+      # Finds the case-insensitive table name to ensure correct retrieval
+      tables <- getTables(connection)
+      caseInsensitiveTableName <- findCaseInsensitiveTable(tables, tableName)
+      if (is.null(caseInsensitiveTableName)) {
+        stop(paste0("The table '", tableName, "' does not exist in the database."))
       }
-    )
+      tableName <- caseInsensitiveTableName
 
-    # Merges the concept IDs with the concept names
-    # This is done to ensure that even concept IDs that are not present in the 'concept' table are included
-    conceptIds <- data.frame(concept_id = conceptIds)
-    conceptCatalog <- merge(conceptIds, conceptCatalog, by = "concept_id", all.x = TRUE)
+      # Attempts to identify the 'concept' table in the database
+      tables <- getTables(connection)
+      conceptTable <- findCaseInsensitiveTable(tables, "concept")
 
-  # In case of an error, closes the database connection and propagates the error
-  }, error = function(error) {
-    closeConnection(connection, error)
-  })
+      # Gets the required column names for this operation
+      columns <- getColumns(connection, tableName)
+      conceptIdColumn <- getConceptIdColumn(tableName)
+
+      # Checks if the concept ID column exists in the table
+      if (!conceptIdColumn %in% columns) {
+        stop(paste0("The column '", conceptIdColumn, "' does not exist in the table '", tableName, "'."))
+      }
+
+      # Retrieves the unique concept IDs from the table
+      conceptIds <- DBI::dbGetQuery(connection, paste0("SELECT DISTINCT ", conceptIdColumn, " FROM ", tableName))
+      conceptIds <- conceptIds[[1]]
+
+      # Retrieves the concepts from the 'concept' table
+      conceptCatalog <- tryCatch(
+        {
+          getConcepts(connection, conceptIds, conceptTable)
+        },
+        # In case of an error, returns an empty data frame (with the same structure as the expected output)
+        error = function(error) {
+          data.frame(concept_id = numeric(0), concept_name = character(0))
+        }
+      )
+
+      # Merges the concept IDs with the concept names
+      # This is done to ensure that even concept IDs that are not present in the 'concept' table are included
+      conceptIds <- data.frame(concept_id = conceptIds)
+      conceptCatalog <- merge(conceptIds, conceptCatalog, by = "concept_id", all.x = TRUE)
+
+      # In case of an error, closes the database connection and propagates the error
+    },
+    error = function(error) {
+      closeConnection(connection, error)
+    }
+  )
 
   # If the retrieval was successful, closes the database connection and returns the concept catalog
   closeConnection(connection)
   return(conceptCatalog)
+}
+
+
+#' Check Privacy Control Level
+#'
+#' This function checks if the current privacy control level is set to either 'permissive' or 'banana'.
+#' It is designed to be called from the DataSHIELD client to verify that the privacy control level is permissive enough to allow the 
+#' required operations used by the package.
+#' If the privacy control level is not set to 'permissive' or 'banana', the resulting error is returned.
+#'
+#' @return An error message if the privacy control level is not set to 'permissive' or 'banana'.
+#'
+#' @export
+#' 
+checkPrivacyControlLevelDS <- function() {
+  tryCatch({
+    dsBase::checkPermissivePrivacyControlLevel(c('permissive', 'banana'))
+  }, error = function(error) {
+    return(error$message)
+  })
+}
+
+
+#' Check Database Connection
+#'
+#' This function checks if a connection to the database can be established using the provided resource.
+#' It is designed to be called from the DataSHIELD client to verify that the database connection is active and valid.
+#'
+#' @param resource An object representing the database resource.
+#'
+#' @export
+#' 
+checkConnectionDS <- function(resource) {
+  connection <- getConnection(resource)
+  closeConnection(connection)
 }
