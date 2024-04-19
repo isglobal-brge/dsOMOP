d9b61d3aec50e6043ac902d5ddcf6fa71cdae8f4
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-04-20 00:53:27 +0200

    feat: convert datetime

--- R/10_types.R
+++ R/10_types.R
@@ -17,18 +17,21 @@ conceptsToFactors <- function(table) {
 }
 
 
-#' Convert Date Columns to Date Type in the Table
+#' Convert Date and DateTime Columns in the Table
 #'
-#' This function takes a data frame and converts all columns that contain "_date" in their name to Date type in R, 
-#' ignoring case.
+#' This function takes a data frame and converts all columns that contain "_datetime" in their name to DateTime type in R,
+#' and all columns that contain "_date" in their name to Date type, ignoring case.
 #'
-#' @param table A data frame with one or more columns that contain "_date" in their name.
+#' @param table A data frame with one or more columns that may contain "_date" or "_datetime" in their name.
 #'
-#' @return A data frame with columns that contain "_date" in their name converted to Date type.
+#' @return A data frame with columns that contain "_datetime" converted to DateTime type and columns that contain "_date" 
+#' converted to Date type.
 #'
 convertDateColumns <- function(table) {
   for (column in names(table)) {
-    if (grepl("_date", column, ignore.case = TRUE)) {
+    if (grepl("_datetime", column, ignore.case = TRUE)) {
+      table[[column]] <- as.POSIXct(table[[column]])
+    } else if (grepl("_date", column, ignore.case = TRUE)) {
       table[[column]] <- as.Date(table[[column]])
     }
   }
