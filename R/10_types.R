9303c93cf0958266c19b80091892463c616dfb01
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-04-21 12:13:54 +0200

    fix: check tokens for date columns conversion

--- R/10_types.R
+++ R/10_types.R
@@ -19,8 +19,9 @@ conceptsToFactors <- function(table) {
 
 #' Convert Date and DateTime Columns in the Table
 #'
-#' This function takes a data frame and converts all columns that contain "_datetime" in their name to DateTime type in R,
-#' and all columns that contain "_date" in their name to Date type, ignoring case.
+#' This function takes a data frame and checks each column name by splitting it at each period ('.'). 
+#' It then converts columns to DateTime type in R if "_datetime" is found in any part of the name except the first token,
+#' and to Date type if "_date" is found under the same conditions, ignoring case.
 #'
 #' @param table A data frame with one or more columns that may contain "_date" or "_datetime" in their name.
 #'
@@ -29,10 +30,15 @@ conceptsToFactors <- function(table) {
 #'
 convertDateColumns <- function(table) {
   for (column in names(table)) {
-    if (grepl("_datetime", column, ignore.case = TRUE)) {
-      table[[column]] <- as.POSIXct(table[[column]])
-    } else if (grepl("_date", column, ignore.case = TRUE)) {
-      table[[column]] <- as.Date(table[[column]])
+    tokens <- unlist(strsplit(column, ".", fixed = TRUE))
+    if (length(tokens) > 1) { # Ensure there's more than one token
+      tokens <- tokens[-1] # Remove the first token
+      joined_tokens <- paste(tokens, collapse = ".") # Re-join the tokens excluding the first one
+      if (grepl("_datetime", joined_tokens, ignore.case = TRUE)) {
+        table[[column]] <- as.POSIXct(table[[column]])
+      } else if (grepl("_date", joined_tokens, ignore.case = TRUE)) {
+        table[[column]] <- as.Date(table[[column]])
+      }
     }
   }
   return(table)
