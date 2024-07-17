adbd7421b136d51a55c6184c7d314f91021d5de9
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-07-17 11:07:30 +0200

    feat: center footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,22 +15,26 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-logos">
-          <a href="https://brge.isglobal.org/" target="_blank" rel="noopener noreferrer">
-            <img src={brgeLogo} alt="BRGE Logo" className="footer-logo" />
-          </a>
-          <a href="https://www.isglobal.org/" target="_blank" rel="noopener noreferrer">
-            <img src={isglobalLogo} alt="ISGlobal Logo" className="footer-logo" />
-          </a>
+        <div className="footer-section">
+          <div className="footer-logos">
+            <a href="https://brge.isglobal.org/" target="_blank" rel="noopener noreferrer">
+              <img src={brgeLogo} alt="BRGE Logo" className="footer-logo" />
+            </a>
+            <a href="https://www.isglobal.org/" target="_blank" rel="noopener noreferrer">
+              <img src={isglobalLogo} alt="ISGlobal Logo" className="footer-logo" />
+            </a>
+          </div>
         </div>
-        <div className="footer-made-by">
+        <div className="footer-section footer-made-by">
           <a href="https://github.com/davidsarratgonzalez" target="_blank" rel="noopener noreferrer" className="footer-link">
             Website made with 🧡 by David Sarrat González
           </a>
         </div>
-        <div className="footer-copyright">
-          © {new Date().getFullYear()} dsOMOP.<br />
-          This project is licensed under the <Link to="/license" className="license-link" onClick={handleMITLicenseClick}><strong>MIT License</strong></Link>.
+        <div className="footer-section">
+          <div className="footer-copyright">
+            © {new Date().getFullYear()} dsOMOP.<br />
+            This project is licensed under the <Link to="/license" className="license-link" onClick={handleMITLicenseClick}><strong>MIT License</strong></Link>.
+          </div>
         </div>
       </div>
     </footer>
