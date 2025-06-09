193f6b897525575c9e33d7f20a2b6c3d4637ed82
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2025-06-09 17:50:55 +0200

    chores: update acknowledgements

--- src/components/Acknowledgements.js
+++ src/components/Acknowledgements.js
@@ -20,21 +20,14 @@ function Acknowledgements() {
         <div className="acknowledgement-card">
           <div className="card-content">
             <p style={justifiedTextStyle}>
-              This project has received funding from the <strong><a href="https://www.ciencia.gob.es/en/" className="highlight-link">Spanish Ministry of Science and Innovation</a></strong> and <strong><a href="https://www.aei.gob.es/en" className="highlight-link">State Research Agency</a></strong> through the <strong>"Centro de Excelencia Severo Ochoa 2019-2023" Program [CEX2018-000806-S]</strong> and <strong><a href="https://www.aei.gob.es/en" className="highlight-link">State Research Agency</a></strong> and <strong><a href="https://ec.europa.eu/regional_policy/funding/erdf_en" className="highlight-link">Fondo Europeo de Desarrollo Regional, UE</a></strong> <strong>(PID2021-122855OB-I00)</strong>, and support from the <strong><a href="https://web.gencat.cat/en/inici/index.html" className="highlight-link">Generalitat de Catalunya</a></strong> through the <strong>CERCA Program</strong> and <strong><a href="https://recercaiuniversitats.gencat.cat/en/inici/" className="highlight-link">Ministry of Research and Universities</a></strong> <strong>(2021 SGR 01563)</strong>.
+              This project has received funding from the <strong><a href="https://www.ciencia.gob.es/en/" className="highlight-link">Spanish Ministry of Education, Innovation and Universities</a></strong>, the <strong><a href="https://www.aei.gob.es/en" className="highlight-link">National Agency for Research</a></strong>, and the <strong><a href="https://ec.europa.eu/regional_policy/funding/erdf_en" className="highlight-link">Fund for Regional Development</a></strong> <strong>(PID2021-122855OB-I00)</strong>. We also acknowledge support from the grant <strong>CEX2023-0001290-S</strong> funded by <strong>MCIN/AEI/10.13039/501100011033</strong>, and support from the <strong><a href="https://web.gencat.cat/en/inici/index.html" className="highlight-link">Generalitat de Catalunya</a></strong> through the <strong><a href="https://cerca.cat/en/" className="highlight-link">CERCA Program</a></strong> and the <strong>Consolidated Group on HEALTH ANALYTICS (2021 SGR 01563)</strong>.
             </p>
           </div>
         </div>
         <div className="acknowledgement-card">
           <div className="card-content">
             <p style={justifiedTextStyle}>
-              This project has received funding from the <strong>"Complementary Plan for Biotechnology Applied to Health"</strong>, coordinated by the <strong><a href="https://ibecbarcelona.eu/" className="highlight-link">Institut de Bioenginyeria de Catalunya (IBEC)</a></strong> within the framework of the <strong>Recovery, Transformation, and Resilience Plan (C17.I1)</strong> - Funded by the <strong><a href="https://european-union.europa.eu/index_en" className="highlight-link">European Union</a></strong> - <strong><a href="https://next-generation-eu.europa.eu/index_en" className="highlight-link">NextGenerationEU</a></strong>.
-            </p>
-          </div>
-        </div>
-        <div className="acknowledgement-card">
-          <div className="card-content">
-            <p style={justifiedTextStyle}>
-              Special thanks to <strong><a href="https://github.com/ESCRI11" className="highlight-link">Xavier Escribà Montagut</a></strong> for his <strong>invaluable</strong> support in the development process.
+              Additionally, this project has received funding from the <strong><a href="https://www.isciii.es/" className="highlight-link">Instituto de Salud Carlos III (ISCIII)</a></strong> through the project <strong>"PMP21/00090,"</strong> co-funded by the <strong><a href="https://european-union.europa.eu/index_en" className="highlight-link">European Union's</a></strong> <strong>Resilience and Recovery Facility</strong>. It has also been partially funded by the <strong>"Complementary Plan for Biotechnology Applied to Health,"</strong> coordinated by the <strong><a href="https://ibecbarcelona.eu/" className="highlight-link">Institut de Bioenginyeria de Catalunya (IBEC)</a></strong> within the framework of the <strong>Recovery, Transformation, and Resilience Plan (C17.I1)</strong> – Funded by the <strong><a href="https://european-union.europa.eu/index_en" className="highlight-link">European Union</a></strong> – <strong><a href="https://next-generation-eu.europa.eu/index_en" className="highlight-link">NextGenerationEU</a></strong>.
             </p>
           </div>
         </div>
