08cda40728c0517212341847cfc3c7152ba6d919
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2026-03-21 20:33:27 +0100

    chores: clean legacy

--- tests/testthat/_problems/test-sql-render-96.R
+++
@@ -1,6 +0,0 @@
-# Extracted from test-sql-render.R:96
-
-# test -------------------------------------------------------------------------
-sql <- .sql_translate("DATEADD(day, 7, start_date)", "sqlite")
-expect_true(grepl("STRFTIME", sql))
-expect_true(grepl("86400", sql))
